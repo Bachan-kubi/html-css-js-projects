54463f30eb5fd7d3e149538efb2187be928e61d5
Bachan-kubi <felixghagra@gmail.com>
2023-08-05 08:48:18 +0600

    learning css

---
+++ html/table/main.js
@@ -0,0 +1,20 @@
+// let student = {
+//     name: 'John',
+//     age: 30,
+//     isAdmin: false,
+//     courses: ['html', 'css', 'js'],
+//     spouse: null
+//   };
+
+//   let json = JSON.stringify(student);
+//   console.log(json)
+
+// let user  = '{"name":"John","age":30,"isAdmin":false,"courses":["html","css","js"],"spouse":null}';
+
+// let p = JSON.parse(user);
+// console.log(p)
+
+let user = '{"name":"john","age":20,"email":"bacha@gmail.com"}'
+
+let newform = JSON.parse(user);
+console.log(newform)
